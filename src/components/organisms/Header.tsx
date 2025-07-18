2101f5d8e5ac201551bb1c29b69191d4a6e70b1f
firazhafiz <firazfulvianhafiz05@gmail.com>
2025-07-18 15:07:23 +0700

    fix header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 import NavbarHome from "./NavbarHome";
 import Hero from "../atoms/Hero";
 
 const Header: React.FC = () => {
   return (
     <div>
-      <NavbarHome />
+      <Suspense fallback={null}>
+        <NavbarHome />
+      </Suspense>
       <Hero />
     </div>
   );
