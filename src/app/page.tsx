c748dd614876bdde294e2909716eb3325d0d2982
firazhafiz <firazfulvianhafiz05@gmail.com>
2025-07-18 15:20:17 +0700

    add dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import MainTemplate from "../components/templates/MainTemplate";
 import { fetchRecipes } from "@/data/fetchRecipes";
-import { AuthProvider } from "@/context/AuthContext";
+import dynamic from "next/dynamic";
+
+const AuthProvider = dynamic(
+  () => import("@/context/AuthContext").then((mod) => mod.AuthProvider),
+  { ssr: false }
+);
 
 export default async function Home() {
   const recipes = await fetchRecipes();
